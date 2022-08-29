fe8bcf6090217162cfdc87fa863d366801badc3e
richard onyett <onyettr@gmail.com>
2022-08-28 20:43:04 -0700

    added header ident parse and print

--- elfpy.py
+++ elfpy.py
@@ -6,45 +6,111 @@
 import sys
 import struct
 import argparse
+from numba.tests.test_print import print_string
 #from builtins import False, True
 
 # 0.0.1    Initial concept + realization
 TOOL_VERSION = "0.0.1"
 
-class elf:
+class elf_parser:
+    """
+        encapsulate the parser
+        store teh data from headers
+    """
+    MAGIC_ELF_MARKER = 0x74
+    EI_CLASS = 0x4
+    EI_DATA = 0x5
+    EI_VERSION = 0x6
+    EI_OSABI = 0x7
+    EI_ABIVERSION = 0x8
+    EI_PAD = 0x9
+
     def __init__(self):
         self.fileName = None
-        self.versbose_mode = False
-        
+        self.verbose_mode = False
         self.e_ident = 0
-    
-    def trace(self,trace_string=None, trace_enable= False):
-        if trace_enable is False:
+        self.e_magic_0 = 0
+        self.e_magic_1 = 0
+        self.e_magic_2 = 0
+        self.e_magic_3 = 0
+        self.e_class = 0
+        self.e_data = 0
+        self.e_version = 0
+        self.e_osabi = 0
+        self.e_abiversion = 0
+
+    def trace(self,trace_string=None, trace_enable=False):
+        if trace_enable is False or self.verbose_mode is False:
             return
 
         sys.stdout.write("[INFO] {}\n".format(trace_string))
-        
-    def set_verbose_mode(self, verbosity=True):
-        self.versbose_mode = verbosity
 
-    def process_line(self, elf_file_handle):
-        self.trace("process_line", True)
-
-        return 0
-
-    def header_ident_read(self, elf_file_handle):
+    def print_string(self,fmt,*args):
         """
-            read first 16 bytes of header (e_ident)
+            like a python printf()
+        """
+        a = fmt % args
+        sys.stdout.write(a)
+
+    def set_file_name(self, fileName=None):
+        """
+            
+        """
+        self.fileName = fileName
+
+    def set_verbose_mode(self, verbosity=True):
+        self.verbose_mode = verbosity
+
+    def get_verbose_mode(self):
+        return self.verbose_mode
+
+    def is_elf(self):
+        """
+            check header to see if this really an ELF file
+        """
+        return (self.e_magic_0 == MAGIC_ELF_MARKER and
+                (self.e_magic_1 == ord('E') and 
+                 self.e_magic_2 == ord('L') and 
+                 self.e_magic_3 == ord('F')) 
+               )
+
+    def header_ident_parse(self, elf_file_handle):
+        """
+            read first 9 bytes of header (e_ident)
         """
         self.trace("<process_header> Starts", True)
 
         self.e_ident = struct.unpack('16B',elf_file_handle.read(16))
-        print(self.e_ident)
+        self.trace(self.e_ident,True)
+
+#        self.e_magic_0 = self.e_ident[0]
+#        self.e_magic_1 = self.e_ident[1]
+#        self.e_magic_2 = self.e_ident[2]
+#        self.e_magic_3 = self.e_ident[3]
+        self.e_magic_0,self.e_magic_1,self.e_magic_2, self.e_magic_3 = self.e_ident[:4]
+
+        self.e_class,self.e_data,self.e_version,self.e_osabi = self.e_ident[4:8]
+
         self.trace("<process_header> Ends", True)
 
         return 0
 
     def header_ident_show(self):
+        """
+            print out the ident header details
+        """
+        self.print_string("ELF File Header: %s\n", self.fileName)
+        self.print_string("EI_MAGIC      %02x %02x ('%c') %02x ('%c') %02x ('%c')\n",
+                          self.e_magic_0, 
+                          self.e_magic_1, chr(self.e_magic_1),
+                          self.e_magic_2, chr(self.e_magic_2),
+                          self.e_magic_3, chr(self.e_magic_3))
+        self.print_string("EI_CLASS      %02x\n", self.e_class)
+        self.print_string("EI_DATA       %02x\n", self.e_data)
+        self.print_string("EI_VERSION    %02x\n", self.e_version)
+        self.print_string("EI_OSABI      %02x\n", self.e_osabi)
+        self.print_string("EI_ABIVERSION %02x\n", self.e_abiversion)
+
         return 0
 
 def check_environment():
@@ -65,7 +131,7 @@ def file_open(elf_file_name):
     file_handle = None
     try:
         file_handle = open(elf_file_name,"rb")
-    except FileNotFoundError:
+    except FileNotFoundErrorverbose:
         sys.stderr.write('[ERROR] File not found {}'.format(elf_file_name))
     except Exception as error:
         sys.stderr.write('[ERROR] {}'.format(error))
@@ -96,9 +162,15 @@ def main():
         print(TOOL_VERSION)
         sys.exit(1)
 
+    print(args.files)
+
     elf_file_handle = file_open(args.files)
-    elfer = elf()
-    elfer.header_ident_read(elf_file_handle)
-    
+    elfer = elf_parser()
+    elfer.set_verbose_mode(args.verbose)
+    elfer.set_file_name(args.files)
+    elfer.header_ident_parse(elf_file_handle)
+
+    elfer.header_ident_show()
+
 if __name__ == "__main__":
     main()
